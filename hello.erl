5136c70bc00fc1be1ac20b2c58ce9cca70cd0ba5
Helder de Sousa <helderhenri@gmail.com>
2024-07-31 15:19:04 -0300

    chore: add function into beam to return integer

--- hello.erl
+++ hello.erl
@@ -1,5 +1,6 @@
 -module(hello).
--export([world/0, bigger/0]).
+-export([world/0, bigger/0, one/0]).
 
 world()->"hello world".
+one()->1.
 bigger()-> -10000000001000000000000002000002000010000000000000001.
